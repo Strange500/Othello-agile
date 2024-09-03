79602a86da2af931f5bc5395f194733ef799cc19
Thomas GUISLIN <thomas.guislin.etu@frene13.iut-infobio.priv.univ-lille1.fr>
2024-09-03 14:27:23 +0200

    Controle de saisie

--- src/main/java/Othello.java
+++ src/main/java/Othello.java
@@ -15,7 +15,7 @@ public class Othello {
         Scanner sc = new Scanner(System.in);
         int[] tab = new int[2];
         System.out.println("Veuillez saisir la ligne de la case choisie (A - H) : ");
-        tab[0] = toInt(sc.next().charAt(0));
+        tab[0] = toInt(sc.next().toUpperCase().charAt(0));
         System.out.println("Veuillez saisir la colonne de la case choisie (1 - 8): ");
         tab[1] = sc.nextInt() -1;
         return tab;
@@ -70,7 +70,10 @@ public class Othello {
             System.out.println(othello.plateau.toString());
             int[] tab = selectionCase(othello.plateau);
             System.out.println(tab[0] + " " + tab[1]);
-            othello.plateau.setCase(Couleur.NOIR, tab[0], tab[1]);
+            try{
+                othello.plateau.setCase(Couleur.NOIR, tab[0], tab[1]);
+            }
+            catch(IndexOutOfBoundsException e){System.out.println("Mauvaise entrée !");}
             System.out.flush();
         }
   
