4ca206b981317bd932a1203e089703d237912bf3
Benjamin Roget <benjamin.roget.etu@univ-lille.fr>
2024-09-03 10:47:54 +0200

    Update file Othello.java

--- src/main/java/Othello.java
+++ src/main/java/Othello.java
@@ -7,4 +7,21 @@ public class Othello {
     public Othello() {
         this.plateau = new Grille();
     }
+
+    public static int[] selectionCase(Grille g ) {
+        Scanner sc = new Scanner(System.in);
+        int[] tab = new int[2];
+        System.out.println("Veuillez saisir la ligne de la case choisie (A - H) : ");
+        tab[0] = toInt(sc.next().charAt(0));
+        System.out.println("Veuillez saisir la colonne de la case choisie (1 - 8): ");
+        tab[1] = sc.nextInt() -1;
+        return tab;
+        
+    }
+
+    private static int toInt(char c) {
+        return c - 'A';
+    }
+
+    
 }
