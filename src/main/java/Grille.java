d27fd4027308281b926438a95cfb285d25ed2213
Cristobal Pinto~ <cristobal.pinto.etu@univ-lille.fr>
2024-09-03 11:21:45 +0200

    Affichage de la grille

--- src/main/java/Grille.java
+++ src/main/java/Grille.java
@@ -18,4 +18,36 @@ class Grille {
         }
     }
 
+    @Override
+    public String toString() {
+        String res = "";
+        res += "┌───┬───┬───┬───┬───┬───┬───┬───┐\n";
+        for(int i=0;i<8;i++){
+            res += "│ ";
+            if(grille.get(0).get(i).couleur == Couleur.NOIR){
+                res += "⛀ ";
+            } else if(grille.get(0).get(i).couleur == Couleur.BLANC){
+                res += "⛂ ";
+            } else {
+            res += "  ";
+            }
+        }
+        for(int i=0;i<8;i++){
+            res += "│\n├───┼───┼───┼───┼───┼───┼───┼───┤\n";
+            for(int j=0;j<8;j++){
+                res += "│ ";
+                if(grille.get(i).get(j).couleur == Couleur.NOIR){
+                    res += "⛀ ";
+                } else if(grille.get(i).get(j).couleur == Couleur.BLANC){
+                    res += "⛂ ";
+                } else {
+                res += "  ";
+                }
+            }
+        }
+        res += "│\n└───┴───┴───┴───┴───┴───┴───┴───┘\n";
+        return res;
+    
+    }
+
 }
